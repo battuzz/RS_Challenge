68b08285363f85430fba8b8973c6d6ef279e6d8c
fabiochiusano <chiusanofabio@hotmail.it>
2017-10-03 19:22:52 +0200

    made preprocess.py importable

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,39 +5,6 @@ import pandas as pd
 import scipy as sc
 import pickle
 
-
-train = pd.read_csv('../data/train_final.csv', delimiter='\t')
-playlists = pd.read_csv('../data/playlists_final.csv', delimiter='\t')
-target_playlists = pd.read_csv('../data/target_playlists.csv', delimiter='\t')
-target_tracks = pd.read_csv('../data/target_tracks.csv', delimiter = '\t')
-tracks = pd.read_csv('../data/tracks_final.csv', delimiter='\t')
-
-
-
-albums = {}
-for _,row in tracks.iterrows():
-    album = row['album'][1:-1]
-    if album != '' and album != 'None':
-        if album in albums:
-            albums[album].append(row['track_id'])
-        else:
-            albums[album] = [row['track_id']]
-
-with open('../data/albums.dat', 'wb') as f:
-    pickle.dump(albums, f)
-
-owners = {}
-for _,row in playlists.iterrows():
-    owner = row['owner']
-    if owner in owners:
-        owners[owner].append(row['playlist_id'])
-    else:
-        owners[owner] = [row['playlist_id']]
-
-with open('../data/owners.dat', 'wb') as f:
-    pickle.dump(owners, f)
-
-
 def playlist_track_list(urm):
     playlist_content = {}
     for _,row in urm.iterrows():
@@ -49,30 +16,62 @@ def playlist_track_list(urm):
 
     return playlist_content
 
+if __name__ == "__main__":
+    train = pd.read_csv('../data/train_final.csv', delimiter='\t')
+    playlists = pd.read_csv('../data/playlists_final.csv', delimiter='\t')
+    target_playlists = pd.read_csv('../data/target_playlists.csv', delimiter='\t')
+    target_tracks = pd.read_csv('../data/target_tracks.csv', delimiter = '\t')
+    tracks = pd.read_csv('../data/tracks_final.csv', delimiter='\t')
 
 
-tags = {}
-for _,row in tracks.iterrows():
-    t = list(map(lambda x : x.strip(), row['tags'][1:-1].split(',')))
-    for x in t:
-        if x != '':
-            if x in tags:
-                tags[x].append(row['track_id'])
+    albums = {}
+    for _,row in tracks.iterrows():
+        album = row['album'][1:-1]
+        if album != '' and album != 'None':
+            if album in albums:
+                albums[album].append(row['track_id'])
             else:
-                tags[x] = [row['track_id']]
+                albums[album] = [row['track_id']]
 
-with open('../data/tags.dat', 'wb') as f:
-    pickle.dump(tags, f)
+    with open('../data/albums.dat', 'wb') as f:
+        pickle.dump(albums, f)
+
+    owners = {}
+    for _,row in playlists.iterrows():
+        owner = row['owner']
+        if owner in owners:
+            owners[owner].append(row['playlist_id'])
+        else:
+            owners[owner] = [row['playlist_id']]
+
+    with open('../data/owners.dat', 'wb') as f:
+        pickle.dump(owners, f)
 
 
 
-artists = {}
-for _,row in tracks.iterrows():
-    artist_id = row['artist_id']
-    if artist_id in artists:
-        artists[artist_id].append(row['track_id'])
-    else:
-        artists[artist_id] = [row['track_id']]
 
-with open('../data/artists.dat', 'wb') as f:
-    pickle.dump(artists, f)
+    tags = {}
+    for _,row in tracks.iterrows():
+        t = list(map(lambda x : x.strip(), row['tags'][1:-1].split(',')))
+        for x in t:
+            if x != '':
+                if x in tags:
+                    tags[x].append(row['track_id'])
+                else:
+                    tags[x] = [row['track_id']]
+
+    with open('../data/tags.dat', 'wb') as f:
+        pickle.dump(tags, f)
+
+
+
+    artists = {}
+    for _,row in tracks.iterrows():
+        artist_id = row['artist_id']
+        if artist_id in artists:
+            artists[artist_id].append(row['track_id'])
+        else:
+            artists[artist_id] = [row['track_id']]
+
+    with open('../data/artists.dat', 'wb') as f:
+        pickle.dump(artists, f)
